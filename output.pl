a8211d3890f50b7cb97f81f19e3c8a1fdee84981
brownbull <carcamo.gabriel@gmail.com>
2017-12-28 10:53:14 -0300

    initial commit with hello and ouhtput

---
+++ output.pl
@@ -0,0 +1,5 @@
+print "this is a text line\n";
+
+$x = 5;
+print "double quotes allow the variable named x to show its real value x = $x\n";
+print 'but within single-quotes its value of $x is not replaced\n';
